457ffcd1f55dc0a4018c75f4c1c13e1ec67a7fa2
Marcello Pedrosa <49072390+marcellopedrosa@users.noreply.github.com>
2025-06-18 14:18:31 -0300

    Update Application.java

--- src/main/java/br/com/empresa/Application.java
+++ src/main/java/br/com/empresa/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@ComponentScan(basePackages = "br.com.empresa")
+@EntityScan(basePackages = "br.com.empresa")
 public class Application {
 
 	public static void main(String[] args) {
